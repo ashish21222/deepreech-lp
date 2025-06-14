5cdcfded03c1f82dd9bcd6f0e5e3362629e833b1
ashish gahlot <gahlotashish21@gmail.com>
2025-06-14 12:22:28 -0400

    FIX: basepath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const repo = 'deepreech-lp'; // your repo name
+
+module.exports = {
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  // ...other config
+};
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
