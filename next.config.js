517ca99da3cbb311f6776c97f20ab67b2d74d3a4
Ashish Gahlot <gahlotashish21@gmail.com>
2026-03-09 11:05:51 +0530

    Remove duplicate next.config.js in favor of next.config.ts

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: ['192.168.128.60'],
-  images: {
-    domains: ['192.168.128.60'],
-  },
-}
-
-module.exports = nextConfig 
